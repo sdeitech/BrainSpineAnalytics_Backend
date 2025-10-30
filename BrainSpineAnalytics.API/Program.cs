00d259427c4baec73416b35d97d363430f56eb6f
Sakshibisht72 <sakshi@smartdatainc.net>
2025-10-30 10:05:30 +0530

    #Fixes

--- BrainSpineAnalytics.API/Program.cs
+++ BrainSpineAnalytics.API/Program.cs
@@ -122,7 +122,7 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
  app.UseSwagger();
  app.UseSwaggerUI();
