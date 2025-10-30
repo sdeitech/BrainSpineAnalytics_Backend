5dc8d6460b4b7f6198daab9eacd64d50604d9fe2
Sakshibisht72 <sakshi@smartdatainc.net>
2025-10-30 15:16:28 +0530

    #Server-up

--- BrainSpineAnalytics.API/obj/Debug/net8.0/BrainSpineAnalytics.API.AssemblyInfo.cs
+++ BrainSpineAnalytics.API/obj/Debug/net8.0/BrainSpineAnalytics.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrainSpineAnalytics.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c787d391b3290b16c7030faa0b3f7b32bfaf1cb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ec4532ebdae4fbfdc4b2a3708b5ddb7c6b6175c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrainSpineAnalytics.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrainSpineAnalytics.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
